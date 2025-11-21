099b8177722dd77aff1dc6efbc59a01b672acf19
Jacob Criner <jacob.criner@gmail.com>
2025-11-21 14:27:30 -0600

    nvim: treewalker, refactoring

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -94,6 +94,44 @@ require('lazy').setup({
   -- "Highlight, edit, and navigate code"
   require 'kickstart.plugins.treesitter',
 
+  -- TODO: Put this in its own plugin.
+  {
+    'aaronik/treewalker.nvim',
+    opts = {},
+    config = function()
+      -- movement
+      vim.keymap.set({ 'n', 'v' }, '<C-k>', '<cmd>Treewalker Up<cr>', { silent = true })
+      vim.keymap.set({ 'n', 'v' }, '<C-j>', '<cmd>Treewalker Down<cr>', { silent = true })
+      vim.keymap.set({ 'n', 'v' }, '<C-h>', '<cmd>Treewalker Left<cr>', { silent = true })
+      vim.keymap.set({ 'n', 'v' }, '<C-l>', '<cmd>Treewalker Right<cr>', { silent = true })
+
+      -- swapping
+      vim.keymap.set('n', '<C-S-k>', '<cmd>Treewalker SwapUp<cr>', { silent = true })
+      vim.keymap.set('n', '<C-S-j>', '<cmd>Treewalker SwapDown<cr>', { silent = true })
+      vim.keymap.set('n', '<C-S-h>', '<cmd>Treewalker SwapLeft<cr>', { silent = true })
+      vim.keymap.set('n', '<C-S-l>', '<cmd>Treewalker SwapRight<cr>', { silent = true })
+    end,
+  },
+
+  -- TODO: test and extract into its own thing
+  {
+    'ThePrimeagen/refactoring.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    lazy = false,
+    opts = {},
+    config = function()
+      -- load refactoring Telescope extension
+      require('telescope').load_extension 'refactoring'
+
+      vim.keymap.set({ 'n', 'x' }, '<leader>rr', function()
+        require('telescope').extensions.refactoring.refactors()
+      end)
+    end,
+  },
+
   -- "Neo-tree is a Neovim plugin to browse the file system"
   require 'kickstart.plugins.neo-tree',
 
